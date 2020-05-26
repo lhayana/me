d96eda73e03945c4e5876456709066ce937eef6d
Travis CI <travis@travis-ci.org>
2020-05-26 02:55:20 -0300

    up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
+import photo from './img/eu.png'
 
 function App() {
   return (
-    <div className="App">
-      <h1>HEELLO WORLD</h1>
+    <div className="background">
+      <div className="conteudo container">
+      </div>
+      <div className= "test">
+        <img src={photo} alt="foto" className="rfoto"/>
+        <i class="fab fa-instagram fa-3x"></i>
+        </div>
     </div>
   );
 }
