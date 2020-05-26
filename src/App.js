815ee0af5407815f9a22842967caa1402a7254ed
Travis CI <travis@travis-ci.org>
2020-05-26 19:00:41 -0300

    up

--- src/App.js
+++ src/App.js
@@ -9,11 +9,16 @@ function App() {
         <img src={photo} alt="foto" className="rfoto"/>
         <h1 className="nome">Lhayana Vieira</h1>
         <div class="btn-group botoes" role="group" aria-label="socialmedia">
-          <a href="https://github.com/lhayana" target="_blank" rel="noopener noreferrer"><button type="button" class="btn btn-secondary btn-dark"><i class="fab fa-github fa-2x"></i> </button></a>   
-          <a href="https://www.linkedin.com/in/lhayana/" target="_blank" rel="noopener noreferrer"><button type="button" class="btn btn-secondary btn-dark"><i class="fab fa-linkedin fa-2x"></i></button> </a>   
-          <a href="https://twitter.com/lhayanavm" target="_blank" rel="noopener noreferrer"><button type="button" class="btn btn-secondary btn-dark"><i class="fab fa-twitter fa-2x"></i></button> </a>   
-          <a href="https://www.facebook.com/lhayana" target="_blank" rel="noopener noreferrer"><button type="button" class="btn btn-secondary btn-dark"><i class="fab fa-facebook fa-2x"></i></button> </a>   
-          <a href="https://www.instagram.com/lhayana/" target="_blank" rel="noopener noreferrer"><button type="button" class="btn btn-secondary btn-dark"><i class="fab fa-instagram fa-2x"></i></button> </a>   
+          <a href="https://github.com/lhayana" target="_blank" rel="noopener noreferrer">
+            <button type="button" class="botao btn btn-secondary btn-info btn-sm"><i class="fab fa-github-square fa-2x"></i> </button></a>   
+          <a href="https://www.linkedin.com/in/lhayana/" target="_blank" rel="noopener noreferrer">
+            <button type="button" class="botao btn btn-secondary btn-info btn-sm"><i class="fab fa-linkedin fa-2x"></i></button> </a>   
+          <a href="https://twitter.com/lhayanavm" target="_blank" rel="noopener noreferrer">
+            <button type="button" class="botao btn btn-secondary btn-info btn-sm"><i class="fab fa-twitter-square fa-2x"></i></button> </a>   
+          <a href="https://www.facebook.com/lhayana" target="_blank" rel="noopener noreferrer">
+            <button type="button" class="botao btn btn-secondary btn-info btn-sm"><i class="fab fa-facebook-square fa-2x"></i></button> </a>   
+          <a href="https://www.instagram.com/lhayana/" target="_blank" rel="noopener noreferrer">
+            <button type="button" class="botao btn btn-secondary btn-info btn-sm"><i class="fab fa-instagram fa-2x"></i></button> </a>   
         </div>
         </div>
         </div>
